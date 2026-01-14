d0961cefc94b04f55fca2ab5986182bac3205db5
Benjinno <163769337+Benjinno@users.noreply.github.com>
2026-01-14 22:05:21 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,17 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+  Component.Header({
+    links: [
+      { name: "Guides", link: "/Guides" },
+      { name: "Outils", link: "/Outils" },
+      { name: "Jeu de construction", link: "/Construction" },
+      { name: "Référence pour programmeurs", link: "/Reference" },
+      { name: "Contribuer", link: "/Contribuer" },
+    ],
+  }),
+],
   afterBody: [],
   footer: Component.Footer({
     links: {
